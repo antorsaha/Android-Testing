131a7d213d2ab8a1c356897bdc49ef0ddabf0e89
antorsaha <antor145648s@gmail.com>
2024-01-10 19:04:13 +0600

    home work 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation ("com.google.truth:truth:1.2.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.12")
     androidTestImplementation ("com.google.truth:truth:1.2.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/test/java/com/sikderithub/androidtesting/HomeWork1Test.kt
@@ -0,0 +1,58 @@
+package com.sikderithub.androidtesting
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+
+class HomeWork1Test{
+
+    @Test
+    fun fib0Returns0(){
+        val result = HomeWork1.fib(0)
+
+        assertThat(result).isEqualTo(0)
+    }
+
+    @Test
+    fun fib1Returns1(){
+        val result = HomeWork1.fib(1)
+
+        assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    fun fib9Returns34(){
+        val result = HomeWork1.fib(9)
+
+        assertThat(result).isEqualTo(21)
+    }
+
+    @Test
+    fun fib10Returns55(){
+        val result = HomeWork1.fib(10)
+
+        assertThat(result).isEqualTo(34)
+    }
+
+    @Test
+    fun invalidBracesReturnsFalse(){
+        val result = HomeWork1.checkBraces("(a * b))")
+
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun invalidBracesReturnsFalse2(){
+        val result = HomeWork1.checkBraces("((a * b)")
+
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validBracesReturnsTrue(){
+        val result = HomeWork1.checkBraces("((a * b))")
+
+        assertThat(result).isTrue()
+    }
+
+}
